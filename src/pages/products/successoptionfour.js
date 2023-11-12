93ab2c8a398658b9611cced4cd385522267517d1
AndreaGodsey <noirmenace101@gmail.com>
2023-11-12 17:23:58 -0500

    Removing Apostrophe
    Removing Apostrophe's

--- src/pages/products/successoptionfour.js
+++ src/pages/products/successoptionfour.js
@@ -7,7 +7,7 @@ export default function ProductsSuccess(){
 
         <h1 className={styles.messageSent}>Thank you for your Purchase!</h1>
         <h1 className={styles.messageSent}>You have ordered 300 cupcakes!</h1>
-        <h1 className={styles.messageSent}>For: Johnny's Open House</h1>
+        <h1 className={styles.messageSent}>For:Open House on Cobb Street</h1>
         <h1 className={styles.messageSent}>Kind of Cupcakes: Chocolate and Strawberry</h1>
         <h1 className={styles.messageSent}>Price:200$</h1>
         <h1 className={styles.messageSent}>Date:November 29th, 2023</h1>
--- src/pages/products/successoptiontwo.js
+++ src/pages/products/successoptiontwo.js
@@ -7,7 +7,7 @@ export default function ProductsSuccess(){
 
         <h1 className={styles.messageSent}>Thank you for your Purchase!  </h1>
         <h1 className={styles.messageSent}>You have ordered 200 cookies!</h1>
-        <h1 className={styles.messageSent}>For: Riley's Birthday Party</h1>
+        <h1 className={styles.messageSent}>For:Birthday Party for Riley</h1>
         <h1 className={styles.messageSent}>Kind of Cookies: Chocolate Chip</h1>
         <h1 className={styles.messageSent}>Price:150$</h1>
         <h1 className={styles.messageSent}>Date:December 14th, 2023</h1>
